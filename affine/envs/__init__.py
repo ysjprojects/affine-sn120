5fc7e103a352de7e91fcea9b0a183504a72b7df3
unconst <jake@bittensor.com>
2025-09-03 16:47:38 +0200

    restart

--- affine/envs/__init__.py
+++ affine/envs/__init__.py
@@ -7,7 +7,7 @@ from typing import Dict, Type
 from . import sat as _sat
 from . import abd as _abd
 from . import ded as _ded
-# from . import elr as _elr
+from . import elr as _elr
 from . import hvm as _hvm
 
 __all__ = []
